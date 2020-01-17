751b761e374d5c6740c4d67e4e15fe4c57cfd6c4
Preethi <preethisv0@gmail.com>
2020-01-17 16:51:19 +0530

    Added comments

--- LongestBitonicSubarray.py
+++ LongestBitonicSubarray.py
@@ -1,4 +1,4 @@
-def LongestBitonicSubarrayLength(X, m):
+def LongestBitonicSubarray(X, m):
     LIS = [0 for _ in range(m)]
     LDS = [0 for _ in range(m)]
 
@@ -6,14 +6,14 @@ def LongestBitonicSubarrayLength(X, m):
     LIS[0] = 1
     for i in range(1, m):
         LIS[i] = 1
-        if X[i - 1] < X[i]:
+        if X[i - 1] < X[i]: # only if prev number is smaller than current
             LIS[i] = LIS[i - 1] + 1
 
     # from last number
     LDS[m - 1] = 1
     for i in reversed(range(m - 1)):
         LDS[i] = 1
-        if X[i] > X[i + 1]:
+        if X[i] > X[i + 1]: # only if next number is smaller than current
             LDS[i] = LDS[i + 1] + 1
 
     longestBSLength = 1
@@ -21,12 +21,14 @@ def LongestBitonicSubarrayLength(X, m):
     for i in range(0, m):
         if longestBSLength < LDS[i] + LIS[i] - 1:
             longestBSLength = LDS[i] + LIS[i] - 1
-            beg = i - LIS[i] + 1
-            end = i + LDS[i] - 1
+            beg = i - LIS[i] + 1 # current index - length of LIS at i
+            end = i + LDS[i] - 1 # current index + length of LDS at i
 
     return longestBSLength, X[beg : end + 1]
 
 
 X = [3, 5, 8, 4, 5, 9, 10, 8, 5, 3, 4]
 m = len(X)
-print(LongestBitonicSubarrayLength(X, m))
+length, subarray = LongestBitonicSubarray(X, m)
+print(length)
+print(subarray)
